97a6fd1ccc492b4e0f8d9c6df7313589493db167
mtbtiago <tiago.ameller@gmail.com>
2016-08-06 19:39:02 +0200

    create new todo view

--- app/models/todo.js
+++ app/models/todo.js
@@ -1,5 +1,10 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-
+  title: DS.attr('string'),
+  body: DS.attr('body'),
+  date: DS.attr('date'),
+  created_at: DS.attr('string', function() {
+    return new Date();
+  })
 });
