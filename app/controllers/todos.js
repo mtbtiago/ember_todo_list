ea8ce38f8a92ba423566b31fdc062a5a3aeb8746
mtbtiago <tiago.ameller@gmail.com>
2016-08-06 21:26:23 +0200

    add filtering

--- app/controllers/todos.js
+++ app/controllers/todos.js
@@ -1,6 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  filter: '',
+  filteredTodos: function() {
+    var filter = this.get('filter');
+    var regexp = new RegExp(filter, 'gi');
+    var todos = this.model;
+
+    return todos.filter(function(todo) {
+      return todo.get('title').match(regexp) || todo.get('body').match(regexp);
+    });
+  }.property('arrangedContent', 'filter'),
   sortedProperties: ['date:asc'],
   sortedTodos: Ember.computed.sort('model', 'sortedProperties')
 });
