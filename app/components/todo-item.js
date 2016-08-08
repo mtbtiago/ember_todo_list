25f2c9c27503fda51ca0e633369a945db4cabe9b
mtbtiago <tiago.ameller@gmail.com>
2016-08-08 19:36:12 +0200

    add toggleCompletion action

--- app/components/todo-item.js
+++ app/components/todo-item.js
@@ -2,8 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   actions: {
-    markAsCompleted: function(todo) {
-      todo.set('completed', true); // locallly
+    toggleCompleted: function(todo) {
+      todo.set('completed', !todo.get('completed')); // locallly
       todo.save(); // remote db
     }
   }
